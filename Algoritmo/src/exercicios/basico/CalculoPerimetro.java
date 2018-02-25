323b9a080d89794de2259681768922ff7fab6df9
Thainá Costa <thainaribeiro96.tr@gmail.com>
2018-02-25 20:06:54 -0300

    Resolvendo exercicio de Calculo de Perimetro

---
+++ Algoritmo/src/exercicios/basico/CalculoPerimetro.java
@@ -0,0 +1,12 @@
+package exercicios.basico;
+
+public class CalculoPerimetro {
+
+	public static void main(String[] args) {		
+				
+		double diametro = 7.0;
+		double pi = 3.14;
+		
+		System.out.println("O perímetro do círculo é " + diametro * pi);		
+	}
+}
